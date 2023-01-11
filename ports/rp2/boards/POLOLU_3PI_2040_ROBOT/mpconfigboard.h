a4e269099216b3222979113b41e15eaa7e7b52ea
Paul Grayson <pdg@alum.mit.edu>
2023-01-10 21:52:31 -0800

    rp2: Add support for Pololu 3pi+ 2040 Robot.
    Includes support for required port args in i2c, spi, and uart libraries.
    Increases USB string descriptor length from 20 to 40 chars.
    
    Signed-off-by: Paul Grayson <paul@pololu.com>

---
+++ ports/rp2/boards/POLOLU_3PI_2040_ROBOT/mpconfigboard.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Pololu 3pi+ 2040 Robot"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (15 * 1024 * 1024)
+
+#define MICROPY_HW_USB_MSC (1)
+#define MICROPY_HW_USB_VID (0x1FFB)
+#define MICROPY_HW_USB_PID (0x2043)
+#define MICROPY_HW_USB_MANUFACTURER_STRING "Pololu Corporation"
+#define MICROPY_HW_USB_PRODUCT_FS_STRING MICROPY_HW_BOARD_NAME " MicroPython"
+
+#define MICROPY_BANNER_MACHINE MICROPY_HW_BOARD_NAME
